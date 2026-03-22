30797c120011554ea6ae1b88a4290bbcc76c55ec
Katw24 <100807935+Katw24@users.noreply.github.com>
2026-03-21 22:07:13 -0300

    k

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './app.tsx'
+import './index.css'
 
-function Teste() {
-  return <h1 style={{ color: 'red' }}>TESTE RENDER</h1>
+const rootElement = document.getElementById('root')
+
+if (!rootElement) {
+  throw new Error("Root element not found")
 }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Teste />
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
